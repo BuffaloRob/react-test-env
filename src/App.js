9d33bf73b60bf7500d7b31d9ebde426c94eefe59
BuffaloRob <peckham.rob@gmail.com>
2019-08-23 14:58:43 -0400

    App: turned into container component holding state and actions for Comment and VisitorName functional components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,53 @@
 import React from 'react';
 import Comment from './/Comment';
 import './App.css';
+import VisitorName from './VisitorName';
+import FizzBuzz from './FizzBuzz';
+import CommentClass from './CommentClass';
+import ItemList from './ItemList';
 
 class App extends React.Component {
 
+  state = {
+    visitorName: '',
+    comment: '',
+    comments: []
+
+  }
+
+  updateVisitorName = name => {
+    this.setState({ visitorName: name})
+  }
+
+  updateComment = comment => {
+    this.setState({ comment: comment})
+  }
+
+  submitComment = comm => {
+    let comment = comm
+    this.setState({ comments: [...this.state.comments, comment] })
+    this.setState({ comment: '' })
+  }
+
   render() {
     return (
-      <Comment />
+      <>
+        <Comment 
+          comment={this.state.comment}
+          comments={this.state.comments}
+          updateComment={this.updateComment}
+          submitComment={this.submitComment}
+        />
+        <VisitorName
+          visitorName={this.state.visitorName}
+          updateVisitorName={this.updateVisitorName}
+        />
+        {/* <FizzBuzz /> */}
+
+        <CommentClass />
+
+        <ItemList />
+      </>
     )
   }
 
